9deb71f1fb026fe15b2d60d4e8cb225f637f9896
zhang_jia_yong <zhangjiayong@zuzuche.com>
2018-04-06 23:27:10 +0800

    隐藏头部

--- src/screens/Customer/index.js
+++ src/screens/Customer/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 class Customer extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+
   componentDidMount() {
     // TODO
   }
--- src/screens/HomePage/index.js
+++ src/screens/HomePage/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 class HomePage extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+
   componentDidMount() {
     // TODO
   }
