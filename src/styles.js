13a988f27d6dd3cf193303f96eeceee1ae4ce997
zhang_jia_yong <zhangjiayong@zuzuche.com>
2018-04-07 17:21:34 +0800

    定义styles.js模块结构

---
+++ src/styles.js
@@ -0,0 +1,41 @@
+/**
+ * 主题样式
+ */
+
+
+export const COLOR = {
+  primary: '',
+  info: '',
+  success: '',
+  error: '',
+  highlight: '',
+  warning: '',
+  normal: '#d9d9d9'
+};
+
+export const PADDING = {
+  lg: '',
+  md: '',
+  sm: '',
+  xs: ''
+};
+
+export const FONT_SIZE = {
+  lg: '',
+  md: '',
+  sm: '',
+  xs: ''
+};
+
+export const TEXT_COLOT = '';
+
+export const BODY_BG_COLOR = '';
+
+export const COMPONENT_BG_COLOR = '';
+
+export const ITEM_BG_COLOR = '';
+
+export const LINE_COLOR = '';
+
+export const BORDER_RADIUS = '';
+
