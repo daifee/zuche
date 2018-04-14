7a31ab5291a6970763eef9d1396dc08843c59d79
zhang_jia_yong <zhangjiayong@zuzuche.com>
2018-04-14 12:17:18 +0800

    集成react-native-venctor-icons

---
+++ docs/troubleshooting.md
@@ -0,0 +1,15 @@
+# Troubleshooting
+
+---
+
+## react-native-vector-icons
+
+### 加载模块失败
+
+信息:
+
+> bundling failed: Error: While resolving module `react-native-vector-icons/Ionicons`, the Haste packa
+
+解决：
+
+> https://github.com/oblador/react-native-vector-icons/issues/626#issuecomment-357405396
--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import {
   StackNavigator,
   TabNavigator,
   TabBarBottom,
 } from 'react-navigation';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomePage from './screens/HomePage';
 import Customer from './screens/Customer';
 import UserProfile from './screens/UserProfile';
@@ -22,6 +23,22 @@ const Root = TabNavigator({
     screen: UserProfile,
   },
 }, {
+  navigationOptions: ({ navigation }) => ({
+    // eslint-disable-next-line
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'HomePage') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+      } else {
+        iconName = `ios-options${focused ? '' : '-outline'}`;
+      }
+
+      // You can return any component that you like here! We usually use an
+      // icon component from react-native-vector-icons
+      return <Ionicons name={iconName} size={25} color={tintColor} />;
+    },
+  }),
   initialRouteName: 'HomePage',
   tabBarOptions: {
     activeTintColor: 'tomato',
