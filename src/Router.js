5f7efcb45d1e348e28eb44e79074b17818a59f38
zhang_jia_yong <zhangjiayong@zuzuche.com>
2018-04-14 12:43:55 +0800

    定义好漂亮的icon

--- src/Router.js
+++ src/Router.js
@@ -4,13 +4,19 @@ import {
   TabNavigator,
   TabBarBottom,
 } from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import HomePage from './screens/HomePage';
 import Customer from './screens/Customer';
 import UserProfile from './screens/UserProfile';
 import SelectCity from './screens/SelectCity';
 import SelectSite from './screens/SelectSite';
 import Search from './screens/Search';
+
+const tabBarIconMap = {
+  HomePage: 'home',
+  Customer: 'earphones-alt',
+  UserProfile: 'user'
+};
 
 const Root = TabNavigator({
   HomePage: {
@@ -27,16 +33,11 @@ const Root = TabNavigator({
     // eslint-disable-next-line
     tabBarIcon: ({ focused, tintColor }) => {
       const { routeName } = navigation.state;
-      let iconName;
-      if (routeName === 'HomePage') {
-        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-      } else {
-        iconName = `ios-options${focused ? '' : '-outline'}`;
-      }
+      const iconName = tabBarIconMap[routeName] || 'settings';
 
       // You can return any component that you like here! We usually use an
       // icon component from react-native-vector-icons
-      return <Ionicons name={iconName} size={25} color={tintColor} />;
+      return <SimpleLineIcons name={iconName} size={25} color={tintColor} />;
     },
   }),
   initialRouteName: 'HomePage',
