77c62893e0f07f770352300ac52c68fb60cd18cf
zhang_jia_yong <zhangjiayong@zuzuche.com>
2018-06-24 03:15:33 +0800

    在Provider同级注册接口

--- src/App.js
+++ src/App.js
@@ -1,55 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import AppNavigation from './AppNavigation';
-// import { View, Text } from 'react-native';
 
 import store from './store';
 
-// import { PopupBottom } from './components/Popup';
-// import { AnimatedPopupBottomContainer } from './components/AnimatedPopup';
 import { AnimatedDatePickerApi } from './components/AnimatedDatePicker';
 
 
-// class Demo extends React.Component {
-//   state = {
-//     visible: true,
-//     date: new Date()
-//   };
-
-//   show() {
-//     this.setState({ visible: true });
-//   }
-
-//   hide() {
-//     this.setState({ visible: false });
-//   }
-
-//   render() {
-//     const { visible, date } = this.state;
-
-//     return (
-//       <AnimatedDatePickerContainer
-//         date={date}
-//         visible={visible}
-//         onCancel={() => {
-//           console.log('cancel');
-//         }}
-//         onConfirm={(date) => {
-//           this.setState({ date: date, visible: false });
-//         }}
-//       />
-//     );
-//   }
-// }
-
 function App() {
   return (
-    <Provider store={store}>
-      <React.Fragment>
+    <React.Fragment>
+      <Provider store={store}>
         <AppNavigation />
-        {AnimatedDatePickerApi.register()}
-      </React.Fragment>
-    </Provider>
+      </Provider>
+      {AnimatedDatePickerApi.register()}
+    </React.Fragment>
   );
 }
 
