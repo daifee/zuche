b161ecd42e2fc15726335129c6b132784fc0846d
zhang_jia_yong <zhangjiayong@zuzuche.com>
2018-06-26 20:26:21 +0800

    渲染sidemenu

---
+++ src/screens/SelectCity/SideMenu/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ScrollView } from 'react-native';
+import SideMenuComponent, { SideMenuItem } from '../../../components/SideMenu';
+import StructCollection from '../../../models/Struct.Collection';
+import Text from '../../../components/Text';
+
+export default function SideMenu(props) {
+  const { categories, selectedId } = props;
+
+  return (
+    <SideMenuComponent>
+      <ScrollView>
+        {categories.map((category) => {
+          const key = `${category.id}`;
+          const selected = category.id === selectedId;
+
+          return (
+            <SideMenuItem key={key} selected={selected}>
+              <Text>{category.name}</Text>
+            </SideMenuItem>
+          );
+        })}
+      </ScrollView>
+    </SideMenuComponent>
+  );
+}
+
+SideMenu.propTypes = {
+  categories: PropTypes.instanceOf(StructCollection).isRequired,
+  selectedId: PropTypes.number
+};
--- src/screens/SelectCity/index.js
+++ src/screens/SelectCity/index.js
@@ -5,24 +5,33 @@
  * * CityList
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import { connect } from 'react-redux';
 import { SearchBarPlaceholder } from '../../components/SearchBar';
-import RegionList from './RegionList';
+import SideMenu from './SideMenu';
 import CityList from './CityList';
 import { SelectCity as styles } from './styles';
 
-import { dispatch } from './store';
+import { dispatch, getState } from './store';
+import StructCollection from '../../models/Struct.Collection';
 
 class SelectCity extends React.Component {
   static navigationOptions = () => ({
     title: '选择城市'
   });
 
+  static propTypes = {
+    categorizeCities: PropTypes.instanceOf(StructCollection).isRequired
+  };
+
   componentDidMount() {
     dispatch({ type: 'categorizeCities/get' });
   }
 
   render() {
+    const { categorizeCities } = this.props;
+
     return (
       <View style={styles.container}>
         <View style={styles.searchBarWrapper}>
@@ -30,7 +39,7 @@ class SelectCity extends React.Component {
         </View>
         <View style={styles.listWrapper}>
           <View style={styles.regionListWrapper}>
-            <RegionList />
+            <SideMenu categories={categorizeCities} />
           </View>
           <View style={styles.cityListWrapper}>
             <CityList />
@@ -41,4 +50,7 @@ class SelectCity extends React.Component {
   }
 }
 
-export default SelectCity;
+export default connect(() => {
+  const { categorizeCities } = getState();
+  return { categorizeCities };
+})(SelectCity);
