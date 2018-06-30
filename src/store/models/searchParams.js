937dedc72101813f5446349e677a794daa9d9d63
zhang_jia_yong <zhangjiayong@zuzuche.com>
2018-06-30 16:56:24 +0800

    设置city

--- src/store/models/searchParams.js
+++ src/store/models/searchParams.js
@@ -1,4 +1,6 @@
 import SearchParamsModel from '../../models/SearchParams.Model';
+import CityModel from '../../models/City.Model';
+
 
 const pickupDate = new Date();
 pickupDate.setDate(pickupDate.getDate() + 1);
@@ -12,14 +14,29 @@ export const searchParams = {
   }),
 
   reducers: {
-    setPickupDate(state, date) {
+    setPickupDate(state, date: Date) {
       const newState = state.clone();
       newState.pickupDate = date;
       return newState;
     },
-    setDropoffDate(state, date) {
+
+    setDropoffDate(state, date: Date) {
       const newState = state.clone();
       newState.dropoffDate = date;
+      return newState;
+    },
+
+    setPickupCity(state, city: CityModel) {
+      const newState = state.clone();
+      newState.pickupCity = city;
+      newState.pickupLandmark = city.landmark;
+      return newState;
+    },
+
+    setDropoffCity(state, city: CityModel) {
+      const newState = state.clone();
+      newState.DropoffCity = city;
+      newState.dropoffLandmark = city.landmark;
       return newState;
     }
   }
