793e81ff35516aa0c40d86a0e9aeb4eea3598da1
zhang_jia_yong <zhangjiayong@zuzuche.com>
2018-07-10 22:49:18 +0800

    打开搜车列表页

--- src/screens/HomePage/SearchSection/SearchButton.js
+++ src/screens/HomePage/SearchSection/SearchButton.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import { Button } from 'react-native-elements';
+import { withNavigation } from 'react-navigation';
 import { SearchButton as styles } from './styles';
 
-export default function SearchButton(props) {
+function SearchButton(props) {
+  const { navigation, ...rest } = props;
   return (
     <View style={styles.container}>
       <Button
-        {...props}
+        {...rest}
         title="搜 索"
         buttonStyle={styles.button}
         titleStyle={styles.title}
+        onPress={() => {
+          navigation.push('SearchCar');
+        }}
       />
     </View>
   );
 }
 
+SearchButton.propTypes = {
+  navigation: PropTypes.instanceOf(Object).isRequired
+};
+
+export default withNavigation(SearchButton);
